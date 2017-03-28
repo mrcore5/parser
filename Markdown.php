ea49b0515c427b6f941eaa4821cac2c62aaa27ba
Matthew Reschke <mail@mreschke.com>
2017-03-28 12:41:29 -0600

    Added mrcore backticks like tt

--- Markdown.php
+++ Markdown.php
@@ -25,10 +25,25 @@ class Markdown extends GithubMarkdown
      */
     protected function renderTable($block)
     {
-        return str_replace(
-            "<table>",
-            "<table class='table table-condensed table-bordered table-striped table-hover dataTable no-footer'>",
-            parent::renderTable($block)
-        );
+        // Calling return parent::renderTable($block); didn't return the proper results for some reason
+        // So this is basically a copy of cebe/markdown/block/TableTrait.php wiht added cable class for bootstrap
+        // I did NOT add dataTable class
+
+        $content = '';
+        $this->_tableCellAlign = $block['cols'];
+        $content .= "<thead>\n";
+        $first = true;
+        foreach($block['rows'] as $row) {
+            $this->_tableCellTag = $first ? 'th' : 'td';
+            $align = empty($this->_tableCellAlign[$this->_tableCellCount]) ? '' : ' align="' . $this->_tableCellAlign[$this->_tableCellCount++] . '"';
+            $tds = "<$this->_tableCellTag$align>" . trim($this->renderAbsy($this->parseInline($row))) . "</$this->_tableCellTag>"; // TODO move this to the consume step
+            $content .= "<tr>$tds</tr>\n";
+            if ($first) {
+                $content .= "</thead>\n<tbody>\n";
+            }
+            $first = false;
+            $this->_tableCellCount = 0;
+        }
+        return "<table class='table table-condensed table-bordered table-striped table-hover'>\n$content</tbody>\n</table>\n";
     }
 }
---
+++ TextWiki/Render/Xhtml/Backticks2.php
@@ -0,0 +1,30 @@
+<?php
+
+class Text_Wiki_Render_Xhtml_Backticks2 extends Text_Wiki_Render
+{
+    public $conf = array(
+        'css' => null,
+        'use_code' => false,
+    );
+
+    public function token($options)
+    {
+        $useCode = $this->getConf('use_code');
+        if ($options['type'] == 'start') {
+            $css = $this->formatConf('class="%s"', 'css');
+            if ($useCode) {
+                return "<code $css>";
+            } else {
+                return "<tt $css>";
+            }
+        }
+
+        if ($options['type'] == 'end') {
+            if ($useCode) {
+                return '</code>';
+            } else {
+                return '</tt>';
+            }
+        }
+    }
+}
